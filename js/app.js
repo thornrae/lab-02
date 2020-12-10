7b168de90d96a8d846374adce0a6fd2582414639
Taylor Thornton <thornrae@gmail.com>
2020-12-09 21:07:22 -0800

    second feature

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ $.ajax('/data/page-1.json')
       //instantiate inside ajax call?
       let newHorn = new Display(hornObject);
       newHorn.renderjquery();
+      newHorn.list();
     });
   });
 
@@ -39,9 +40,31 @@ Display.prototype.renderjquery = function () {
   let photoTemplate = $('#photo-template').html();
   let $newTemplate = $(`<section>${photoTemplate}</section>`);
   $newTemplate.find('img').attr('src', this.image);
+  $newTemplate.find('img').attr('alt', this.title);
+  $newTemplate.find('h2').text(this.keyword);
+  $newTemplate.addClass(this.keyword);
+  $newTemplate.find('p').text(this.description);
   $('main').append($newTemplate);
   console.log($newTemplate);
 };
+
+Display.prototype.list = function () {
+  const $keyword = $(`<option value="${this.keyword}">${this.keyword}</option>`);
+  $('select').append($keyword);
+  $('select').on('change', function (){
+    //grabs value of thing selected, stores in const
+    const keyword = $(this).val();
+    //hides all sections
+    $('section').hide();
+    //shows any item with the selected keyword as its class
+    $(`.${keyword}`).show();
+    if(keyword === 'default'){
+      $('section').show();
+    }
+  });
+};
+
+
 
 console.log(hornArray);
 //instantiate each object in the json file using constructor
