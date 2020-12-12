c4b70b6dfb514d3b43e0188348b651fa8a9d0d82
Taylor Thornton <thornrae@gmail.com>
2020-12-12 11:20:42 -0800

    clean up in prep for lab3

--- README.md
+++ README.md
@@ -21,3 +21,34 @@ Time Estimate
   - Start time: 9:05
   - Finish Time: 9:30
   - Actual time needed to complete: 25 minutes
+
+
+# lab-03
+
+Contributors: Taylor Thornton, Cody Carpenter
+
+Time Estimate
+
+### Feature #1: Pagination
+  - Estimate of time needed to complete: 30-45 minutes
+  - Start time: 
+  - Finish Time: 
+  - Actual time needed to complete: 
+
+### Feature #2: Templating
+  - Estimate of time needed to complete: 1 hour
+  - Start time: 8:30
+  - Finish Time: 9:05
+  - Actual time needed to complete: 35 minutes
+
+### Feature #3: Styling with Flexbox
+  - Estimate of time needed to complete: 1 hour
+  - Start time: 9:05
+  - Finish Time: 9:30
+  - Actual time needed to complete: 25 minutes
+
+### Feature #4: Sort Images
+  - Estimate of time needed to complete: 1-2 hour
+  - Start time: 9:05
+  - Finish Time: 9:30
+  - Actual time needed to complete: 25 minutes
--- js/app.js
+++ js/app.js
@@ -3,28 +3,16 @@
 let hornArray = [];
 
 
-// let $photoTemplate = $('#photo-template');
-
 //link json file
 $.ajax('/data/page-1.json')
   .then( data => {
-    //this separates each array data index into its own object, this makes it easier to manipulate each data point
     data.forEach(hornObject => {
-
       console.log(hornObject);
-      //next, select element from DOM
-      //clone?
-      //remove or add any attributes
-      //add content using text(value.WHATEVERPROPERTYNAME)
-      //instantiate inside ajax call?
       let newHorn = new Display(hornObject);
       newHorn.renderjquery();
       newHorn.list();
     });
   });
-
-//grab DOM elements and assign to variable
-
 
 function Display (object) {
   this.image= object.image_url;
@@ -64,10 +52,7 @@ Display.prototype.list = function () {
   });
 };
 
-
-
 console.log(hornArray);
-//instantiate each object in the json file using constructor
-// page-1.forEach(new MakeImage)
+
 
 
